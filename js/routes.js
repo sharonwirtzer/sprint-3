acbcb6d131cc3a7d00967f70ab1085dada02d847
ShoshE2 <shoshelsa111@gmail.com>
2021-02-24 18:04:49 +0200

    h

--- js/routes.js
+++ js/routes.js
@@ -1,5 +1,5 @@
 import homePage from './pages/home-page.cmp.js';
-import emailApp from './apps/mail/pages/email-app.cmp.js';
+import emailApp from './apps/email/pages/email-app.cmp.js';
 import keepApp from './apps/keep/pages/keep-app.cmp.js';
 import about from './pages/about.cmp.js';
 import book from './apps/book/pages/book-app.cmp.js';
