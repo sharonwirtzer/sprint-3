3d1d02895118382f82741b3af9deae44cd0b3f15
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-24 23:32:44 +0200

    .

--- js/apps/keep/services/list-service.js
+++ js/apps/keep/services/list-service.js
@@ -1,8 +1,20 @@
 export const keepList =[
     {
       "id": "OXeMG8wNskc",
-      "title": "bhbhbhbhhbhhbh",
-      "note": "mi est eros convallis auctor arcu dapibus himenaeos",
+      "title": "Groceries",
+      "note": "- 1kg tomatoes\n- 1kg tomatoes ",
+
+    },
+    {
+      "id": "OXeMG8wNskc",
+      "title": "livres à lire",
+      "note": "- Les Fleurs du Mal",
+
+    },
+    {
+      "id": "OXeMG8wNskc",
+      "title": "Daphné´s birthday",
+      "note": "26/05/1994 - remember to go pick up flowers and make a Charlotte cake!",
 
     },
    
