760f2564f8049f540d897a7aa2e60257db8ba622
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-24 17:59:16 +0200

    email

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
 
 import homePage from './pages/home-page.cmp.js';
-import emailApp from './apps/mail/pages/email-app.cmp.js';
+import emailApp from './apps/email/pages/email-app.cmp.js.js';
  import keepApp from './apps/keep/pages/keep-app.cmp.js';
 import about from './pages/about.cmp.js';
 import book from './apps/book/pages/book-app.cmp.js';
