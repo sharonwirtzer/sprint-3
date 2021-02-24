6d2d4d8bd7a107345b33778db4f4c35ce105eb00
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-24 23:53:43 +0200

    -

--- js/cmps/app-header.cmps.js
+++ js/cmps/app-header.cmps.js
@@ -5,7 +5,7 @@ export default {
         <nav>
            <router-link active-class="active-link" to="/" exact>Home</router-link> |
            <router-link to="/email">Email</router-link> |
-           <router-link to="/keep">Keep</router-link> |
+           <router-link to="/keep">Keep  </router-link> |
            <router-link to="/book">Books</router-link> |
            <router-link to="/about">About</router-link>
 
