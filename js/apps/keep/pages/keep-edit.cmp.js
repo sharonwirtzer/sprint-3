49ae5f48283ecc3c90b3291d7bb862a4cc96dfb9
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-25 20:57:50 +0200

    ,

--- js/apps/keep/pages/keep-edit.cmp.js
+++ js/apps/keep/pages/keep-edit.cmp.js
@@ -6,17 +6,19 @@ export default {
         <form @submit.prevent="save">
        
 
-            <input type= "file" @change= "onfileSelected">
             <input type="text" class="text-area" placeholder="Take a note..." v-model="keepToEdit.title"    >
             <button title="New  list"><img src="img/10.png" height="30" width="30" /></button>
+
             <button title="New note with image"><img src="img/11.png" height="30" width="30" /></button>
+
+             <input type= "file" @change= "onfileSelected" >
+
             <button title="Save"><img src="img/8.png" height="30" width="30" /></button>
 
 
         </form>
       </section>   
-        <!--  <textarea rows="15" cols="30" name="comment"      form="usrform" placeholder="Take a note..." v-model="keepToEdit.title"></textarea>  -->
-  
+        
     `,
     data() {
         return {
@@ -30,7 +32,6 @@ export default {
         onfileSelected(event){
            this.selectedFile =  event.target.files[0]
         },
-
 
         save() {
             keepService.save(this.keepToEdit)
