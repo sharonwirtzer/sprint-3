fb681809fffaaaa498424a1929a271a9e7def2a9
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-25 13:06:47 +0200

    -

--- js/apps/keep/cmps/keep-list.cmp.js
+++ js/apps/keep/cmps/keep-list.cmp.js
@@ -5,23 +5,17 @@ export default {
     props: ['keeps'],
     template: `
     <ul class="keep-list">
-
-
     
         <li v-for="keep in keeps" :key="keep.id" class="keep-preview-container" >
             <keep-preview  :keep="keep"  @click.native="logId(keep.id)"  />
           
-                <button @click="remove(keep.id)">X</button>
-
-
-
+            <button @click="remove(keep.id)">X</button>
 
                 <div class="color">
-  <input type="text" v-bind:style="{ color: color}" v-model="color" maxlength="7" placeholder="#000000"/>
-  <input type="color" v-model="color"/>
+           <input type="text" v-bind:style="{ color: color}" v-model="color" maxlength="7" placeholder="#000000"/>
+           <input type="color" v-model="color"/>
     </div>
 
-         
 
         </li>
     </ul>
