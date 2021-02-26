cdccc13c07deaee1cc5f2f8acadfed696df419d1
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-27 00:18:50 +0200

    css email

--- js/apps/email/cmps/side-nav.cmp.js
+++ js/apps/email/cmps/side-nav.cmp.js
@@ -12,9 +12,9 @@ export default {
 
     template: `
     <section class="side-nav">
-            <router-link to="/email/add" tag="button">+Compose</router-link>
-            <router-link to="/email/inbox"  tag="button">Inbox</router-link>
-            <router-link to="/email/sent"  tag="button">Sent</router-link>                   
+            <router-link to="/email/add" tag="button"><img src="img/plus.png" height="20" width="20" /> Compose</router-link>
+            <router-link to="/email/inbox"  tag="button"><img src="img/inbox.png" height="20" width="20" /> Inbox</router-link>
+            <router-link to="/email/sent"  tag="button"><img src="img/sent.png" height="20" width="20" /> Sent</router-link>                   
     </section>
     `
 }
