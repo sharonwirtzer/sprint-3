d71b6a9856dbff502204d9df98c4a2948261b8ed
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-26 23:05:02 +0200

    css

--- js/cmps/app-header.cmps.js
+++ js/cmps/app-header.cmps.js
@@ -8,8 +8,8 @@ export default {
           <!--  <router-link active-class="active-link" to="/" exact>Home</router-link> | -->
            <router-link to="/email">Email</router-link> |
            <router-link to="/keep">Keep  </router-link> |
-           <router-link to="/book">Books</router-link> |
-           <router-link to="/about">About</router-link>
+           <router-link to="/book">Books</router-link> <!-- |
+           <router-link to="/about">About</router-link> -->
 
        </nav>
     </header>
--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -2,6 +2,18 @@ export default {
     template: `
     <section class="home app-main">
         <h1>Apsus</h1>
-    </section>
+
+<section class="openn"   >
+
+<router-link  class="open" to="/email"> <img src="img/emailgg.png" height="80" width="80" /></router-link> 
+<router-link    class="open"  to="/keep"><img src="img/noteg.png" height="80" width="80" />  </router-link> 
+<router-link   class="open" to="/book"> <img  src="img/bookg.png" height="80" width="80" /></router-link> 
+
+</section>  
+</section>    
     `
 }
+
+
+
+{/* <router-link to="/email">Email</router-link> */}
