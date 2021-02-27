49689f026acd4396081366d8e45e526a417a9dd4
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-27 17:01:47 +0200

    keep

--- js/apps/keep/services/list-service.js
+++ js/apps/keep/services/list-service.js
@@ -1,10 +1,5 @@
 export const keepList =[
-    {
-      "id": "OXeMG8wNskc",
-      "title": "buy food for louis",
-      "img": "/img/2.jpeg"
-
-    },
+ 
     {
       "id": "OXeMG8wNskc",
       "title": "livres à lire",
@@ -16,6 +11,28 @@ export const keepList =[
       "id": "OXeMG8wNskc",
       "title": "Daphné´s birthday",
       "note": "26/05/1994 - remember to go pick up flowers and make a Charlotte cake!",
+     
+
+    },
+
+      {
+      "id": "OXeMG8wNskc",
+      "title": "Password",
+      "note": "Amazon: hgdg7xt",
+     
+
+    },
+    {
+      "id": "OXeMG8wNskc",
+      "title": "Buy food for Louis",
+      "img": "/img/2.jpeg"
+
+    },
+  
+    {
+      "id": "OXeMG8wNskc",
+      "title": "Shelves size",
+      "note": "100x25x3cm",
      
 
     },
