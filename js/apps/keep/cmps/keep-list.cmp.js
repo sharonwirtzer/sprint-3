82a084bb2d51385ef9fb8dce0ddbdce063da3d80
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-27 18:00:41 +0200

    color

--- js/apps/keep/cmps/keep-list.cmp.js
+++ js/apps/keep/cmps/keep-list.cmp.js
@@ -6,18 +6,28 @@ export default {
     template: `
     <ul class="keep-list">
     
-        <li v-for="keep in keeps" :key="keep.id" class="keep-preview-container"  >
-            <keep-preview  :keep="keep"  @click.native="logId(keep.id)"  />
+        <li v-for="keep in keeps" :key="keep.id" class="keep-preview-container" v-bind:style="{ backgroundColor: color}"> 
+            <keep-preview  :keep="keep"  @click.native="logId(keep.id)"  />   
+            <button  title="Delete" @click="remove(keep.id)"><img src="img/50.png" height="20" width="20" /></button>  
             <div class="color">
-            <input  type="color" v-model="color" />       
-            <button  title="Delete" @click="remove(keep.id)"><img src="img/50.png" height="30" width="30" /></button>  
-          
+            <input type="color" v-model="color"/>
+     </div>
+
     </div>
 
 
         </li>
     </ul>
     `,
+
+    data() {
+       
+            return {
+              color: '# rgb(245, 246, 247)'
+            }
+          
+    },
+
     methods: {
         remove(keepId) {
             this.$emit('remove', keepId)
@@ -33,7 +43,7 @@ export default {
 
 
     components: {
-        keepPreview
+        keepPreview,
     }
 
 }
