3da947c481cd745ba05a8ff2d50a98c658c9e521
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-27 23:01:50 +0200

    ,

--- js/apps/keep/pages/keep-edit.cmp.js
+++ js/apps/keep/pages/keep-edit.cmp.js
@@ -7,9 +7,8 @@ export default {
         <form @submit.prevent="save">
             <input type="text" class="text-area" placeholder="Take a note..." v-model="keepToEdit.title" >
             <button title="New  list"><img src="img/10.png" height="30" width="30" /></button>
-            <button title="New note with image"><img src="img/11.png" height="30" width="30" /></button>
+            <img src="img/11.png" height="30" width="30">
             <button title="Save"><img src="img/8.png" height="30" width="30" /></button>
-            <img src: previewImage class="uploading-image" />
               <div>
    </div>
               
