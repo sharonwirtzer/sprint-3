6ed5e2efefb202a9575e76fe91254edf4ba3c5c2
sharonwirtzer <sharonwirtzer@gmail.com>
2021-02-28 03:00:01 +0200

    n

--- js/apps/email/cmps/side-nav.cmp.js
+++ js/apps/email/cmps/side-nav.cmp.js
@@ -1,15 +1,24 @@
 export default {
     template: `
     <section class="side-nav">
-        <router-link  class="side-nav1"  to="/email/add" tag="button" @click.native="openCompose()">Compose<img src="img/plus.png" height="30" width="30" /></router-link>
-        <router-link to="/email/inbox"  tag="button"> Inbox<img src="img/inbox.png" height="20" width="20" /></router-link>
-        <router-link to="/email/sent"  tag="button">Sent  <img src="img/sent.png" height="20" width="20" /> </router-link>                   
+        <button class="side-nav1" v-on:click="sent">Compose <img src="img/plus.png" height="30" width="30" /></button>
+        <button @click=" sentAt ? one() : two()">Inbox <img src="img/sent.png" height="20" width="20" /></button>
+        <button @click=" reciveAt ? two() : one()">Sent <img src="img/inbox.png" height="20" width="20" /></button>
+     
     </section>
     `,
-    method: {
-        openCompose() {
-            this.$emit('openCompose');
+ 
+    computed:{
+        one() {
+            console.log('one');
+
+        },
+        two() {
+            console.log('two');
+
         }
 
-    }
+        
+    },
+  
 }
